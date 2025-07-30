7fc7837d8fa225be2ad5ed71eef1024230bcc1b9
ahmad <ahmaddabeer36@gmail.com>
2025-07-30 23:58:03 +0530

    ✅ ViewBinding add kiya
    ✅ Home Screen banayi
    
    ✅ RecyclerView me "Water Supply" jaise feature items daale

---
+++ app/src/main/java/com/example/mysocietyhub/ui/home/HomeFragmentAdapter.java
@@ -0,0 +1,73 @@
+package com.example.mysocietyhub.ui.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.mysocietyhub.R;
+
+import java.util.List;
+
+public class HomeFragmentAdapter extends RecyclerView.Adapter<HomeFragmentAdapter.ViewHolder>{
+
+
+
+    public HomeFragmentAdapter(Context context, List<HomeViewModel> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    Context context;
+    List<HomeViewModel> data;
+
+    @NonNull
+    @Override
+    public HomeFragmentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.home_fragment_single_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeFragmentAdapter.ViewHolder holder, int position) {
+
+
+        final HomeViewModel flag = data.get(position);
+
+
+        holder.service.setText(flag.getName());
+        Glide.with(holder.img.getContext())
+                .load(flag.getImage())
+                .into(holder.img);
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView service;
+        ImageView img;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            service = itemView.findViewById(R.id.name);
+            img = itemView.findViewById(R.id.image);
+
+        }
+    }
+}
--- app/src/main/java/com/example/mysocietyhub/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/mysocietyhub/ui/home/HomeViewModel.java
@@ -4,16 +4,33 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class HomeViewModel {
 
-    private final MutableLiveData<String> mText;
+
+    String name,image;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public HomeViewModel(String name, String image) {
+        this.name = name;
+        this.image = image;
     }
 }
